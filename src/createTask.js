32644dd531223c9baa1cf28edb559abe0b82b143
tomasyovino <tomasyovino@gmail.com>
2023-05-20 18:02:39 -0300

    Display simple CRUD

---
+++ src/createTask.js
@@ -0,0 +1,36 @@
+const { v4 } = require('uuid');
+const AWS = require('aws-sdk');
+
+const addTask = async (event) => {
+    try {
+        const dynamodb = new AWS.DynamoDB.DocumentClient();
+    
+        const { title, description } = JSON.parse(event.body);
+        const createdAt = new Date();
+        const id = v4();
+    
+        const newTask = {
+            id,
+            title,
+            description,
+            createdAt,
+            done: false
+        };
+    
+        await dynamodb.put({
+            TableName: 'TaskTable',
+            Item: newTask
+        }).promise();
+    
+        return {
+            status: 200,
+            body: JSON.stringify(newTask)
+        };
+    } catch (error) {
+        console.log(error);
+    };
+};
+
+module.exports = {
+    addTask
+};
